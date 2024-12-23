bc8f41a5d10d4cb492feabef87b770aabd481b42
Tomasz Michalak <vlad3dracula@gmail.com>
2024-12-23 09:57:24 +0100

    poprawki

--- components/AuthProvider.jsx
+++ components/AuthProvider.jsx
@@ -2,23 +2,23 @@
 
 import { createContext, useEffect, useState } from "react";
 import { auth } from "@services/controllerService";
-import { userData as USERDATA, baseUrl} from '../services/commonConsts'
+import { userData as USERDATA, baseUrl } from "../services/commonConsts";
 
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  console.log('AuthProvider')
+  console.log("AuthProvider");
   const [isAuthorized, setIsAuthorized] = useState();
   const [userId, setUserId] = useState();
   const [roles, setRoles] = useState();
   const [token, setToken] = useState();
   const [expiration, setExpiration] = useState();
-  
+
   const setUserData = (userData) => {
     setIsAuthorized(userData?.isAuthorized);
     setUserId(userData?.userId);
     setRoles(userData?.roles);
-    setToken(userData?.token)
+    setToken(userData?.token);
     setExpiration(userData?.expiration);
     sessionStorage.setItem(USERDATA, JSON.stringify(userData));
   };
@@ -27,13 +27,11 @@ const AuthProvider = ({ children }) => {
     const session = sessionStorage.getItem(USERDATA);
     const userData = session && JSON.parse(session);
     setUserData(userData);
-    console.log('checkAuth ', token)
-    if (!token || new Date(expiration) < new Date()){      
-      let data = await logIn(null, null, "Guest");
-        setUserData(data);
-        console.log('checkAuth inside ', data)
-      };
+    console.log("checkAuth ", token);
+    if (!token || new Date(expiration) < new Date()) {
+      await logIn(null, null, "Guest");
     }
+  };
 
   const logIn = async (email, password, authType) => {
     const userData = await auth(baseUrl + "/auth", {
@@ -49,7 +47,7 @@ const AuthProvider = ({ children }) => {
   };
 
   const logOut = () => {
-    con
+    con;
     const userData = {
       isAuthorized: false,
       userId: undefined,
@@ -62,10 +60,21 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     checkAuth();
-  }, [])
+  }, []);
 
   return (
-    <AuthContext.Provider value={{isAuthorized, userId, roles, expiration, token, checkAuth, logIn, logOut }}>
+    <AuthContext.Provider
+      value={{
+        isAuthorized,
+        userId,
+        roles,
+        expiration,
+        token,
+        checkAuth,
+        logIn,
+        logOut,
+      }}
+    >
       {children}
     </AuthContext.Provider>
   );
--- services/controllerService.jsx
+++ services/controllerService.jsx
@@ -1,99 +1,103 @@
-'use client'
+"use client";
 
-import { applicationJson, userData as USERDATA, bearer } from '@services/commonConsts';
+import {
+  applicationJson,
+  userData as USERDATA,
+  bearer,
+} from "@services/commonConsts";
 
-export const auth = async (url, authInput) =>{
-    let data;
-      try {
-        data = await fetch(url, {
-          method: 'POST',
-          headers: { 'Content-Type': applicationJson },
-          body: JSON.stringify(authInput),
-        });
-      } catch (error) {
-        console.error("Fetch error:", error);
-        data = { isRegistered: false, userId: null, token: null, roles: [] };
-      }
+export const auth = async (url, authInput) => {
+  let data;
+  try {
+    data = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(authInput),
+    });
+  } catch (error) {
+    console.error("Fetch error:", error);
+    data = { isRegistered: false, userId: null, token: null, roles: [] };
+  }
 
-      return data.json();
-}
+  return data.json();
+};
 
 export const getProducts = async (url) => {
-    const userData = JSON.parse(localStorage.getItem(USERDATA));
-    const data = await fetch(url, {
-        method: 'GET',
-        headers:  { 
-          'Content-Type': applicationJson,
-          'Authorization': `${bearer} ${userData?.token}`
-        }
-    });
-    return data.json();
-}
+  const userData = JSON.parse(localStorage.getItem(USERDATA));
+  const data = await fetch(url, {
+    method: "GET",
+    headers: {
+      "Content-Type": applicationJson,
+      Authorization: `${bearer} ${userData?.token}`,
+    },
+  });
+  return data.json();
+};
 
 export const getProduct = async (url, id) => {
   const userData = JSON.parse(localStorage.getItem(USERDATA));
-  const data = await fetch(url + '/' + id, {
-    method: 'GET',
-    headers: { 
-      'Content-Type': applicationJson,
-      'Authorization': `${bearer} ${userData?.token}`
-    }
+  const data = await fetch(url + "/" + id, {
+    method: "GET",
+    headers: {
+      "Content-Type": applicationJson,
+      Authorization: `${bearer} ${userData?.token}`,
+    },
   });
 
-  return data.json();
-}
+  return data;
+};
 
 export const addProduct = async (url, product) => {
   const userData = JSON.parse(localStorage.getItem(USERDATA));
   const data = await fetch(url, {
-    method: 'POST',
-    headers:{
-      'Content-Type': applicationJson,
-      'Authorization': `${bearer} ${userData?.token}`
+    method: "POST",
+    headers: {
+      "Content-Type": applicationJson,
+      Authorization: `${bearer} ${userData?.token}`,
     },
-    body: JSON.stringify(product)
+    body: JSON.stringify(product),
   });
 
   return data;
-}
+};
 
-export const deleteProduct = async (url, id) =>{
+export const deleteProduct = async (url, id) => {
   const userData = JSON.parse(localStorage.getItem(USERDATA));
-  const data = await fetch(url + '/' + id, {
-    method: 'DELETE',
-    headers:{
-      'Content-Type': applicationJson,
-      'Authorization': `${bearer} ${userData?.token}`
-    }
-  });
-
-  return data;
-}
-
-export const registerAccount = async (url, inputUser) =>{
-  const userData = JSON.parse(localStorage.getItem(USERDATA));
-  const data = await fetch(url, {
-    method: 'POST',
-    headers:{
-      'Content-Type': applicationJson,
-      'Authorization': `${bearer} ${userData?.token}`
+  const data = await fetch(url + "/" + id, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": applicationJson,
+      Authorization: `${bearer} ${userData?.token}`,
     },
-    body: JSON.stringify(inputUser)
   });
 
   return data;
-}
+};
 
-export const deleteAccount = async (url, deleteGuid) =>{
+export const registerAccount = async (url, inputUser) => {
   const userData = JSON.parse(localStorage.getItem(USERDATA));
   const data = await fetch(url, {
-    method: 'DELETE',
-    headers:{
-      'Content-Type': applicationJson,
-      'Authorization': `${bearer} ${userData?.token}`
+    method: "POST",
+    headers: {
+      "Content-Type": applicationJson,
+      Authorization: `${bearer} ${userData?.token}`,
     },
-    body: JSON.stringify(deleteGuid)
+    body: JSON.stringify(inputUser),
   });
 
   return data;
-}
+};
+
+export const deleteAccount = async (url, deleteGuid) => {
+  const userData = JSON.parse(localStorage.getItem(USERDATA));
+  const data = await fetch(url, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": applicationJson,
+      Authorization: `${bearer} ${userData?.token}`,
+    },
+    body: JSON.stringify(deleteGuid),
+  });
+
+  return data;
+};
